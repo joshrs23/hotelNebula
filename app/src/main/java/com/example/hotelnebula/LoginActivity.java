be9518174985193bf22ff581139a83473aae3c3a
aldo012903 <38449755+aldo012903@users.noreply.github.com>
2023-12-09 16:06:14 -0500

    Welcome functionality

--- app/src/main/java/com/example/hotelnebula/LoginActivity.java
+++ app/src/main/java/com/example/hotelnebula/LoginActivity.java
@@ -3,12 +3,18 @@ package com.example.hotelnebula;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements  View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/example/hotelnebula/WelcomeActivity.java
+++ app/src/main/java/com/example/hotelnebula/WelcomeActivity.java
@@ -2,13 +2,43 @@ package com.example.hotelnebula;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    Button btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initialize();
+    }
+
+    private void initialize() {
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnLogin.setOnClickListener(this);
+        btnSignup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id==R.id.btnLogin) goToLogin();
+        if (id==R.id.btnSignup) goToSignup();
+    }
+
+    private void goToSignup() {
+        Intent intent =new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToLogin() {
+        Intent intent =new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
