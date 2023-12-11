d26f2f91bc5fbac12025a4b8ae28e93497c0d548
AliasTheBlank <aliastheblank@gmail.com>
2023-12-10 19:16:56 -0500

    feat: started implementing sing up view

--- app/src/main/java/com/example/hotelnebula/RegisterActivity.java
+++ app/src/main/java/com/example/hotelnebula/RegisterActivity.java
@@ -2,19 +2,65 @@ package com.example.hotelnebula;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity implements  View.OnClickListener{
+import com.google.android.material.snackbar.Snackbar;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView txtFullName, txtUsername, txtEmail, txtPassword, txtConfirmPassword;
+    Button btnCreateAccount, btnGoTOLogIn;
+    DatabaseReference usersDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initialize();
     }
 
-    @Override
-    public void onClick(View v) {
+    private void initialize() {
+        txtFullName = findViewById(R.id.edFullName);
+        txtUsername = findViewById(R.id.edUsername);
+        txtEmail = findViewById(R.id.edEmail);
+        txtPassword = findViewById(R.id.edPassword);
+        txtConfirmPassword = findViewById(R.id.edConfirmPassword);
 
+        btnGoTOLogIn = findViewById(R.id.btnLogin);
+        btnCreateAccount = findViewById(R.id.btnSignup);
+
+        btnGoTOLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String id = txtFullName.getText().toString();
+                    String name = txtUsername.getText().toString();
+                    String age = txtEmail.getText().toString();
+                    String password = txtPassword.getText().toString();
+                    String confirmPassword = txtConfirmPassword.getText().toString();
+
+                    Person person = new Person(Integer.valueOf(id), name, Integer.valueOf(age));
+                    personDatabase.child(id).setValue(person);
+                    Snackbar.make(view, "The person with id: " + id + " has been added successfully", Snackbar.LENGTH_LONG).show();
+                    clearWidgets();
+                }
+                catch (Exception e ) {
+                    Snackbar.make(view, e.getMessage(), Snackbar.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
