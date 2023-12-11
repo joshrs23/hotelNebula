84fc947e3543b995cd0b932f0c9cdecae0db6940
EmilianoOnTheRunn <emilianoruizgomez@outlook.com>
2023-12-10 23:46:36 -0500

    profile and edit activities

--- app/src/main/java/com/example/hotelnebula/MainActivity.java
+++ app/src/main/java/com/example/hotelnebula/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.hotelnebula;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    Button btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void initialize(){
+    private void initialize() {
+        btnProfile = findViewById(R.id.btnProgile);
+        btnProfile.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id==R.id.btnProgile) goToProfile();
+    }
+
+    private void goToProfile() {
+        Intent intent =new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 
 
