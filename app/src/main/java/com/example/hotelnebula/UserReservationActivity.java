d04a60e2c1ed512d813fc93da11a14745eab8487
jose salazar <joshrs23@gmail.com>
2023-12-11 11:50:20 -0500

    updating reservation view

--- app/src/main/java/com/example/hotelnebula/UserReservationActivity.java
+++ app/src/main/java/com/example/hotelnebula/UserReservationActivity.java
@@ -44,14 +44,19 @@ public class UserReservationActivity extends AppCompatActivity {
     }
 
     private void initialize() {
-        reservationsList = new ArrayList<>();
+        /*reservationsList = new ArrayList<>();
         lvReservation = findViewById(R.id.lvReservations);
         this.checkReservation();
         reservationAdapter = new ReservationAdapter(this,reservationsList);
+        lvReservation.setAdapter(reservationAdapter);*/
+        reservationsList = new ArrayList<>();
+        lvReservation = findViewById(R.id.lvReservations);
+        reservationAdapter = new ReservationAdapter(this, new ArrayList<>());
         lvReservation.setAdapter(reservationAdapter);
+        checkReservation();
     }
 
-    public void checkReservation() {
+    /*public void checkReservation() {
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Reservations");
         SharedPreferences sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
@@ -83,6 +88,42 @@ public class UserReservationActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) {
 
                 Log.e("ReservationAdapter", "Database Error: " + error.getMessage());
+            }
+        });
+    }*/
+
+    public void checkReservation() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Reservations");
+        SharedPreferences sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+
+        Query checkReservationData = reference.orderByChild("User").equalTo(username);
+        ArrayList<Reservations> tempReservationsList = new ArrayList<>();
+
+        checkReservationData.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot reservationSnapshot : snapshot.getChildren()) {
+                        String userFromDB = reservationSnapshot.child("User").getValue(String.class);
+
+                        if (userFromDB.equals(username)) {
+                            int RoomNumber = reservationSnapshot.child("RoomNumber").getValue(Integer.class);
+                            String CheckInDate = reservationSnapshot.child("CheckInDate").getValue(String.class);
+                            String CheckOutDate = reservationSnapshot.child("CheckOutDate").getValue(String.class);
+
+                            Reservations reservation = new Reservations(userFromDB, RoomNumber, CheckInDate, CheckOutDate);
+                            tempReservationsList.add(reservation);
+                        }
+                    }
+
+                    reservationAdapter.reloadData(tempReservationsList);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("UserReservationActivity", "Database Error: " + error.getMessage());
             }
         });
     }
